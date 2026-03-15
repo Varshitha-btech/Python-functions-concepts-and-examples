41492ac4e1b17bfb9ad40db5974a80a3ca2de353
Varshitha <varshitha9408@gmail.com>
2026-03-15 11:04:18 +0530

    Add files via upload

---
+++ area_rec_functions.py
@@ -0,0 +1,7 @@
+def rectangle_area(length,breadth):
+    print("enter the lenght:",length)
+    print("enter the breadth:",breadth)
+    area=length*breadth
+    return area
+result=rectangle_area(5,3)
+print(result)
